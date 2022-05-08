750b4e79c85cb8eed5b5228e54004125a00d84f0
PavelNikolaeW <pavel19.nikolaev93@gmail.com>
2022-05-08 14:33:12 +0300

    feat: finish sprint

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Group, Post
 
 
 def index(request):
-	return HttpResponse("Index")
+    posts = Post.objects.all().order_by('-pub_date')[:10]
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, group):
-	return HttpResponse(f"group_posts {group}")
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        "group": group,
+        'posts': posts
+    }
+    return render(request, 'posts/group_list.html', context)
